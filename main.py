033e495e44a928ce47ad8ec77b50b123569ecfdc
danalzhang <crz1688@gmail.com>
2023-10-12 22:13:32 -0400

    Initial Commit - Added Spotify Recommendation Tool

---
+++ main.py
@@ -0,0 +1,147 @@
+from flask import Flask, redirect, request, jsonify, session
+import requests
+import urllib.parse
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+app.secret_key = '1203120938120481048'
+
+CLIENT_ID = '7de8799313714e4e987d0edf56bc9503'
+CLIENT_SECRET = '070d3e88774d47a8a1d5921ff7bbd2d1'
+REDIRECT_URI = 'http://localhost:8080/callback'
+
+AUTH_URL = "https://accounts.spotify.com/authorize"
+TOKEN_URL = "https://accounts.spotify.com/api/token"
+
+@app.route('/')
+def index():
+    return "Welcome to MusicMan <a href='/login'>Login with Spotify</a>"
+
+@app.route('/login')
+def login():
+    scope = 'user-read-private user-read-email user-top-read'
+    
+    params = {
+        'client_id': CLIENT_ID,
+        'response_type': 'code',
+        'scope': scope,
+        'redirect_uri': REDIRECT_URI,
+        'show_dialog': True
+    }
+    
+    auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
+    
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+    if 'error' in request.args:
+        return jsonify({"error": request.args['error']})
+    
+    if 'code' in request.args:
+        req_body = {
+            'code': request.args['code'],
+            'grant_type': 'authorization_code',
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        }
+        
+        response = requests.post(TOKEN_URL, data=req_body)
+        token_info = response.json()
+        
+        session['access_token'] = token_info['access_token']
+        session['refresh_token'] = token_info['refresh_token']
+        session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
+        
+        return redirect('/recommendations')
+
+@app.route('/recommendations')
+def get_recommendations():
+    if 'access_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        return redirect('/refresh-token')
+
+    # Set up request headers
+    headers = {
+        'Authorization': f"Bearer {session['access_token']}"
+    }
+
+    seed_artists = get_top_artists()
+
+    # Define request parameters
+    params = {
+        'seed_artists': ','.join(seed_artists),
+        'limit': 10,
+        'market': 'ES',
+        ### WHEN IMPLEMENTING COHERE USE MAX/MIN DANCEABILITY AND MAYBE ACOUSTICNESS.
+        'min_danceability': 0.5
+    }
+
+    # Make the request to the Spotify API
+    response = requests.get('https://api.spotify.com/v1/recommendations', headers=headers, params=params)
+    
+    if response.status_code == 200:
+        recommendations = response.json()
+
+        # Extract and format the track names and artists
+        track_info = [{'track_name': track['name'], 'artist_name': track['artists'][0]['name']} for track in recommendations['tracks']]
+        
+        return jsonify(track_info)
+    else:
+        return jsonify({'error': 'Unable to fetch recommendations'}), response.status_code
+
+@app.route('/top_artists')
+def get_top_artists():
+    if 'access_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        return redirect('/refresh-token')
+    
+    headers = {
+        'Authorization': f"Bearer {session['access_token']}"
+    }
+
+    params = {
+        'limit': 3,
+    }
+    
+    # Make the request to the Spotify API
+    response = requests.get('https://api.spotify.com/v1/me/top/artists', headers=headers, params=params)
+    
+    if response.status_code == 200:
+        top_artists = response.json()
+
+        # Extract the artist IDs from the user's top artists
+        seed_artists = [artist['id'] for artist in top_artists['items']]
+        
+        return seed_artists
+    else:
+        return jsonify({'error': 'Unable to fetch top artists'}), response.status_code
+
+@app.route('/refresh-token')
+def refresh_token():
+    if 'refresh_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        req_body = {
+            'grant_type': 'refresh_token',
+            'refresh_token': session['refresh_token'],
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        }
+        
+        response = request.post(TOKEN_URL, data=req_body)
+        new_token_info = response.json()
+        
+        session['access_token'] = new_token_info['access_token']
+        session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in']
+        
+        return redirect('/recommendations')
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ recommendation.py
@@ -0,0 +1,74 @@
+import cohere
+from cohere.responses.classify import Example
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy import Spotify
+
+# Define your client ID, client secret, and redirect URI
+client_id = "7de8799313714e4e987d0edf56bc9503"
+client_secret = "070d3e88774d47a8a1d5921ff7bbd2d1"
+redirect_uri = "https://accounts.spotify.com/en/login?flow_ctx=ff6d1333-3803-4bf3-bb39-203f1eede22c:1697002799"
+
+# Create a SpotifyOAuth instance
+sp_oauth = SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+
+# Get the access token
+access_token_info = sp_oauth.get_cached_token()
+
+# Check if the access token is valid and get a new one if needed
+if not access_token_info:
+    auth_url = sp_oauth.get_authorize_url()
+    print(f"Visit this URL to authorize the application: {auth_url}")
+    response = input("Enter the URL you were redirected to: ")
+    code = sp_oauth.parse_response_code(response)
+    access_token_info = sp_oauth.get_access_token(code)
+
+# Create a Spotify instance using the access token
+sp = Spotify(auth_manager=sp_oauth)
+
+# Search for tracks
+results = sp.search(q='track:dancing', type='track')
+print(results)
+
+
+
+
+
+def recommend_songs(user_response):
+    co = cohere.Client('aYJ8rezd2aYX1737eDEPAKexziGnJb59acmz3Hno')
+    classifications = co.classify(
+        model='embed-english-v2.0',
+        inputs=["I did not fail the test today", "I failed my math test today","4/10"],
+        examples=[
+            Example("I had lots of fun at the park", "positive"), 
+            Example("My friend came over to play Minecraft", "positive"),
+            Example("I passed my exam", "positive"),
+            Example("I am feeling optimistic", "positive"),
+            Example("I got a lot of work done today", "positive"),
+            Example("I am grateful for the opportunities I have", "positive"),
+            Example("10/10", "positive"),
+            Example("I am happy", "positive"),
+            Example("I feel energized", "positive"),
+            Example("I have had a long day", "negative"),
+            Example("I am apprehensive", "negative"),
+            Example("No one came to my thanksgiving dinner", "negative"),
+            Example("My day was good", "negative"),
+            Example("My day was very bad", "negative"),
+            Example("I am not happy", "negative"),
+            Example("I have lots of work that I still have to do", "negative"),
+            Example("I am very stressed", "negative"),
+            Example("I am not feeling very well", "negative"),
+            Example("I am worried", "negative"),
+            Example("I am sad", "negative"),
+            Example("I did not complete my goals for today", "negative"),
+            Example("1/10", "negative")])
+    print('The confidence levels of the labels are: {}'.format(
+        classifications.classifications
+    ))
+    
+    response = co.chat(
+        message="hello"
+    )
+    print(response)
+    
+#recommend_songs("")
